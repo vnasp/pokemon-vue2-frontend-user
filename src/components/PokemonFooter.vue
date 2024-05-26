df886d08e8e126cbd660c37d63930889c80b1930
Valentina Muñoz <valentinamr@gmail.com>
2024-05-25 22:54:15 -0400

    Add footer text

---
+++ src/components/PokemonFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <p>© {{ currentYear }} Todos los derechos reservados</p>
+    <p>
+      Sitio creado con <a href="https://v2.vuejs.org" target="_blank" rel="noopener noreferrer"> Vue2</a> y <a
+        href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">TailwindCSS</a>
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'PokemonFooter',
+  data() {
+    return {
+      currentYear: new Date().getFullYear()
+    };
+  }
+}
+</script>
+
+<style></style>
