21e1739a74b30c6c37cdc03cadcaad8b7368ae9d
Valentina Muñoz <valentinamr@gmail.com>
2024-05-25 22:54:25 -0400

    Update header like component

---
+++ src/components/PokemonHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <div class="header__img">
+      <div class="pokeball"></div>
+      <img src="./../assets/img/logo.png" alt="Pokémon" class="header__img--logo" />
+    </div>
+    <img src="./../assets/img/whois.png" alt="¿Quién es ese Pokémon?" class="header__img--quiz" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'PokemonHeader',
+}
+</script>
+
+<style></style>
